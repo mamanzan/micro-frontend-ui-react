ec7a72a08cd49127faf1979d38c31d697468c99c
Michael Manzano <michael.a.manzano@intel.com>
2022-05-14 19:14:01 -0700

    test better

--- src/component-examples/CheckboxList.tsx
+++ src/component-examples/CheckboxList.tsx
@@ -3,13 +3,30 @@ import { Table } from "../components/tables/Table";
 import { ITableColumn, SortDirection } from "../components/tables/TableColumns";
 import { IComponentExampleConfiguration } from "../interface/ComponentExamples";
 import { IItem } from "../interface/interface";
+import { fruitItems, IFruitItem } from "../models/Fruits";
+import { randomizeIds } from "../utils/RandomizeIds";
 
 interface IFruit extends IItem {
   icon: string;
   name: string;
 }
 
+randomizeIds(fruitItems);
+const fruitRows = new Map<number, IFruitItem>(
+  randomizeIds(fruitItems).map((value: IFruitItem) => [
+    value.id as number,
+    value,
+  ])
+);
+
 const fruitColumns: ITableColumn<IFruit>[] = [
+  {
+    field: "name",
+    type: "string",
+    displayName: "Name",
+    sort: SortDirection.NONE,
+  },
+
   {
     field: "icon",
     type: "string",
@@ -17,20 +34,26 @@ const fruitColumns: ITableColumn<IFruit>[] = [
     sort: SortDirection.NONE,
   },
   {
-    field: "name",
-    type: "string",
-    displayName: "Name",
+    field: "quantity",
+    type: "int",
+    displayName: "Quantity",
+    sort: SortDirection.NONE,
+  },
+  {
+    field: "price",
+    type: "float",
+    displayName: "Price",
     sort: SortDirection.NONE,
   },
 ];
-const fruitRows = new Map<number, IFruit>();
-fruitRows.set(1, { id: 1, value: "kiwi", icon: "🥝", name: "Kiwi" });
-fruitRows.set(2, {
-  id: 2,
-  value: "strawberry",
-  icon: "🍓",
-  name: "Strawberry",
-});
+// const fruitRows = new Map<number, IFruit>();
+// fruitRows.set(1, { id: 1, value: "kiwi", icon: "🥝", name: "Kiwi" });
+// fruitRows.set(2, {
+//   id: 2,
+//   value: "strawberry",
+//   icon: "🍓",
+//   name: "Strawberry",
+// });
 
 export const CheckboxList: IComponentExampleConfiguration[] = [
   {
